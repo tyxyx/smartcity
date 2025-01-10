92a03b61a64669670e6d6aa1ce34d8cb77798f8c
ChanYunWen <yunwen65@gmail.com>
2025-01-11 00:17:37 +0800

    added input txt box?

--- public/app.js
+++ public/app.js
@@ -1,102 +1,102 @@
+console.log("test");
+
 const axios = require("axios");
 const fs = require("fs");
-const pdf = require('pdf-parse');
+const pdf = require("pdf-parse");
 
 const ollamaModel = "granite3.1-dense:8b";
 
 //To query ollama
 async function queryOllama(prompt) {
-    try{
-        const response = await axios.post("http://localhost:11434/api/generate", {
-            model: ollamaModel,
-            prompt: prompt
-        });
-        
-        let fullResponse = '';
-        const chunks = response.data.split('\n');
+  try {
+    const response = await axios.post("http://localhost:11434/api/generate", {
+      model: ollamaModel,
+      prompt: prompt,
+    });
 
-        chunks.forEach(chunk => {
-            if(chunk) {
-                const jsonChunk = JSON.parse(chunk);
-                fullResponse += jsonChunk.response;
-            }
-        })
-        return fullResponse
-    }catch (error) {
-        console.error("Error querying Ollama:", error);
-    }
+    let fullResponse = "";
+    const chunks = response.data.split("\n");
+
+    chunks.forEach((chunk) => {
+      if (chunk) {
+        const jsonChunk = JSON.parse(chunk);
+        fullResponse += jsonChunk.response;
+      }
+    });
+    return fullResponse;
+  } catch (error) {
+    console.error("Error querying Ollama:", error);
+  }
 }
 
 //To generate embedding
 async function generateEmbed(input) {
-    try{
-        const response = await axios.post("http://localhost:11434/api/embed", {
-            model: ollamaModel,
-            input: input
-        });
+  try {
+    const response = await axios.post("http://localhost:11434/api/embed", {
+      model: ollamaModel,
+      input: input,
+    });
 
-        return response.data
-    }catch (error) {
-        console.error("Error generating Embed Ollama:", error);
-    }
+    return response.data;
+  } catch (error) {
+    console.error("Error generating Embed Ollama:", error);
+  }
 }
 
 function cleanText(text) {
-    return text.replace(/[^\x20-\x7E]/g, '').trim(); // Remove non-printable characters
+  return text.replace(/[^\x20-\x7E]/g, "").trim(); // Remove non-printable characters
 }
 
 //Convert PDF to Text
 async function convertPDFToText(filePath) {
-    try{
-        let dataBuffer = fs.readFileSync(filePath) //replace this with filePath later
+  try {
+    let dataBuffer = fs.readFileSync(filePath); //replace this with filePath later
 
-        const data = await pdf(dataBuffer);
+    const data = await pdf(dataBuffer);
 
-        return cleanText(data.text);
-    } catch (error) {
-        console.log("Error converting to text");
-    }
+    return cleanText(data.text);
+  } catch (error) {
+    console.log("Error converting to text");
+  }
 }
 
 //Chunking
-function chunkText(text, maxChunkSize = 300){
-    const sentences = text.split(/(?<=[.!?])\s+/);
-    const chunks = [];
-    let currentChunk = "";
+function chunkText(text, maxChunkSize = 300) {
+  const sentences = text.split(/(?<=[.!?])\s+/);
+  const chunks = [];
+  let currentChunk = "";
 
-    sentences.forEach(sentence => {
-        if ((currentChunk + sentence).length > maxChunkSize) {
-            chunks.push(currentChunk.trim());
-            currentChunk = sentence; // Start a new chunk
-        }else{
-            currentChunk += " " + sentence;
-        }
-    });
-
-    if(currentChunk) {
-        chunks.push(currentChunk.trim());
+  sentences.forEach((sentence) => {
+    if ((currentChunk + sentence).length > maxChunkSize) {
+      chunks.push(currentChunk.trim());
+      currentChunk = sentence; // Start a new chunk
+    } else {
+      currentChunk += " " + sentence;
     }
+  });
 
-    return chunks
+  if (currentChunk) {
+    chunks.push(currentChunk.trim());
+  }
+
+  return chunks;
 }
 
 //Main
 (async () => {
-    const filePath = "./public/doc/government-data-security-policies.pdf";
-    const text = await convertPDFToText(filePath);
-    
-    const chunks = chunkText(text);
-    const embeddings = [];
-    for (const chunk of chunks) {
-        const embedding = await generateEmbed(chunk);
-        embeddings.push({ chunk, embedding });
-    }
-    console.log("Embeddings: ", embeddings);
+  const filePath = "./public/doc/government-data-security-policies.pdf";
+  const text = await convertPDFToText(filePath);
 
+  const chunks = chunkText(text);
+  const embeddings = [];
+  for (const chunk of chunks) {
+    const embedding = await generateEmbed(chunk);
+    embeddings.push({ chunk, embedding });
+  }
+  console.log("Embeddings: ", embeddings);
 
-    // const prompt = "What is the capital of France?";
-    // const response = await queryOllama(prompt);
-    // const embed = await generateEmbed(prompt);
-    // console.log("Response from Ollama:", response);
-
+  const prompt = "What is the capital of France?";
+  const response = await queryOllama(prompt);
+  const embed = await generateEmbed(prompt);
+  console.log("Response from Ollama:", response);
 })();
